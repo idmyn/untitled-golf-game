fb27db8565fed5e8373f8b0cb612d28bcf770a87
Tim Golding <tgolding55@hotmail.com>
2019-11-11 18:32:08 +0000

    added 1 small map

--- server/map.js
+++ server/map.js
@@ -15,7 +15,11 @@ Map.all = {}
 Map.map1 = function(){
   const testMap = new Map()
   const obj1 = new MapObject(100, 100, 200, 20)
+  const obj2 = new MapObject(100, 300, 200, 20)
+  const obj3 = new MapObject(100, 500, 200, 20)
   obj1.addToMap(testMap)
+  obj2.addToMap(testMap)
+  obj3.addToMap(testMap)
   testMap.hole =  {x:200,y:50,radius:20}
   return testMap
 }
