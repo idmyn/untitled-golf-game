8024300e0b6f29e5a465a225bc707dafaf2ecdad
idmyn <hello@davidmyno.rs>
2019-11-11 17:16:47 +0000

    remove socket list for now

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ const io = require('socket.io')(http)
 const Player = require('./server/player')
 const Game = require('./server/game')
 
-const SOCKET_LIST = {}
-
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/client/index.html')
 })
