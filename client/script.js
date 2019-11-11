5cb58935aac4af4f856d74a1199bbc0276bb089f
idmyn <hello@davidmyno.rs>
2019-11-11 09:35:52 +0000

    init socket.io

--- client/script.js
+++ client/script.js
@@ -1,1 +1,3 @@
+const socket = io()
+
 console.log("hi")
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 app.use("/client", express.static("client"))
 const http = require("http").createServer(app)
+const io = require("socket.io")(http)
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/client/index.html")
@@ -9,4 +10,8 @@ app.get("/", function(req, res) {
 
 http.listen(3000, function() {
   console.log("listening on *:3000")
+})
+
+io.on("connection", function(socket) {
+  console.log("a user connected")
 })
