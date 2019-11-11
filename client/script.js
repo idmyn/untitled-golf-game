a2455bd1889bdc54bdda35d828730c9072670a71
idmyn <hello@davidmyno.rs>
2019-11-11 13:15:42 +0000

    bring in matter.js and render on client side

--- client/script.js
+++ client/script.js
@@ -1,3 +1,11 @@
 const socket = io()
 
-console.log('hi')
+const canvas = document.querySelector('#game')
+const ctx = canvas.getContext('2d')
+
+socket.on('position', (ballPos)=> {
+  ctx.clearRect(0,0,400,600)
+  ctx.beginPath()
+  ctx.arc(ballPos.x, ballPos.y, 10, 0, 2 * Math.PI)
+  ctx.stroke()
+})
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+global.window = {} // https://github.com/liabru/matter-js/issues/101#issuecomment-161618366
 const express = require('express')
 const app = express()
 app.use('/client', express.static('client'))
@@ -13,5 +14,42 @@ http.listen(3000, function() {
 })
 
 io.on('connection', function(socket) {
+  const ball = Bodies.circle(200, 500, 15)
+  var ground = Matter.Bodies.rectangle(400, 610, 810, 60, { isStatic: true })
+  World.add(world, [ball, ground])
+
+  playerJoined(ball)
+
   console.log('a user connected')
 })
+
+
+const Matter = require('matter-js/build/matter.js')
+
+const Engine = Matter.Engine,
+  World = Matter.World,
+  Bodies = Matter.Bodies,
+  Body = Matter.Body,
+  Vector = Matter.Vector
+
+// assume dimensions of canvas = 400, 600
+
+const engine = Engine.create()
+const world = engine.world
+// world.gravity.y = 0
+
+Engine.run(engine)
+
+function updateMatter(){
+  Matter.Events.trigger(engine, 'tick', { timestamp: engine.timing.timestamp })
+  Matter.Engine.update(engine, engine.timing.delta)
+  Matter.Events.trigger(engine, 'afterTick', { timestamp: engine.timing.timestamp })
+}
+
+function playerJoined(ball){
+  setInterval(()=>{
+    updateMatter()
+    let ballPos = ball.position
+    io.emit('position', ballPos)
+  }, 1000/25)
+}
