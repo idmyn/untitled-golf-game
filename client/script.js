8bec5d6b9b06053121b06f57c3c0e0a750dff2a0
idmyn <hello@davidmyno.rs>
2019-11-11 10:02:22 +0000

    change eslint to prefer single quotes

--- client/script.js
+++ client/script.js
@@ -1,3 +1,3 @@
 const socket = io()
 
-console.log("hi")
+console.log('hi')
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-app.use("/client", express.static("client"))
-const http = require("http").createServer(app)
-const io = require("socket.io")(http)
+app.use('/client', express.static('client'))
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/client/index.html")
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/client/index.html')
 })
 
 http.listen(3000, function() {
-  console.log("listening on *:3000")
+  console.log('listening on *:3000')
 })
 
-io.on("connection", function(socket) {
-  console.log("a user connected")
+io.on('connection', function(socket) {
+  console.log('a user connected')
 })
