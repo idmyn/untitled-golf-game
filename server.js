cc715b68bac91d56e631682da60f205ce712483a
idmyn <hello@davidmyno.rs>
2019-11-11 09:31:22 +0000

    link up all files in client folder

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const app = express()
+app.use("/client", express.static("client"))
 const http = require("http").createServer(app)
 
-app.get("/", function(req, res){
-  res.send("<h1>Hello world</h1>")
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/client/index.html")
 })
 
-http.listen(3000, function(){
+http.listen(3000, function() {
   console.log("listening on *:3000")
 })
